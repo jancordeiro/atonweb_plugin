ba0870c6f4029f05ac9ea6c39a8f1b3013cfbcf9
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2023-08-30 10:18:48 -0300

    Update get_video_by_day.php
    Only use this PHP script in your current theme's functions.php file.

--- get_video_by_day.php
+++ get_video_by_day.php
@@ -1,27 +1,31 @@
 <?php // APAGUE ESSAA TAG DE ABERTURA SE USAR NO FUNCTIONS.PHP DO SEU TEMA
 
 // INICIO FUNÇÃO VIDEO POR DIA
+
 function get_youtube_video_urls_by_day() {
     // Array com as URLs dos vídeos do YouTube
     $video_urls = array(
-        'domingo' => 'https://www.youtube.com/watch?v=tNfMjI-nkAE',
-        'segunda' => 'https://www.youtube.com/watch?v=kU7b9olDWns',
-        'terca' => 'https://www.youtube.com/watch?v=wURxzw6d98w',
-        'quarta' => 'https://www.youtube.com/embed/gnMDYLrNwFQ',
-        'quinta' => 'https://www.youtube.com/watch?v=zHlxDWuBJ-c',
-        'sexta' => 'https://youtube.com/watch?v=dC0-nQjOsqo',
-        'sabado' => 'https://www.youtube.com/watch?v=KVDKWrsP3es',
+        0 => 'https://www.youtube.com/watch?v=MKM90u7pf3U',
+        1 => 'https://www.youtube.com/watch?v=E6M-XUW4xYY',
+        2 => 'http://www.youtube.com/watch?v=ZIMfB0cHBu8',
+        3 => 'https://www.youtube.com/watch?v=Di310WS8zLk',
+        4 => 'https://www.youtube.com/watch?v=zZdVwTjUtjg',
+        5 => 'https://youtube.com/watch?v=mGgMZpGYiy8',
+        6 => 'https://www.youtube.com/watch?v=YDDHHrt6l4w',
     );
+	
+	// Obtém a data atual do WordPress
+    $current_time = strtotime(current_time('mysql'));
 
     // Obtém o nome do dia da semana em minúsculas (por exemplo: "segunda")
-    $day_of_week = strtolower(date('l'));
+    $day_of_week = date('w', $current_time);
 
     // Verifica se o nome do dia da semana está no array de URLs
     if (isset($video_urls[$day_of_week])) {
         return $video_urls[$day_of_week];
     } else {
         // Retorna uma URL padrão caso não haja URL correspondente ao dia da semana
-        return 'https://www.youtube.com/embed/gnMDYLrNwFQ?si=GpgStrlqHrV20QqD';
+        return 'https://www.youtube.com/watch?v=nVqaintvwFg';
     }
 }
 
@@ -30,9 +34,10 @@ function youtube_video_by_day_shortcode() {
     $video_url = get_youtube_video_urls_by_day();
 
     // Monta o código HTML para exibir o vídeo do YouTube
-    $output = '<div class="youtube-video">';
+    $output = $video_url;
+    /*$output = '<div class="youtube-video">';
     $output .= '<iframe width="560" height="315" src="' . $video_url . '" frameborder="0" allowfullscreen></iframe>';
-    $output .= '</div>';
+    $output .= '</div>'; */
 
     return $output;
 }
