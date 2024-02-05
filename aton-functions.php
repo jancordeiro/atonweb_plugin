a4992d92cdaa0f515191d502e25e4f6606b41bb8
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2024-02-05 15:30:23 -0300

    Update aton-functions.php

--- aton-functions.php
+++ aton-functions.php
@@ -8,16 +8,6 @@
  * URL: https://jancordeiro.github.io
  */
 
-function load_aton_admin_style() {
-    wp_enqueue_style('aton_admin_css', get_stylesheet_directory_uri() . '/aton-admin-style.css');
-}
-add_action('admin_enqueue_scripts', 'load_aton_admin_style');
-
-function load_aton_login_style() {
-    wp_enqueue_style('aton-login-css', get_stylesheet_directory_uri() . '/aton-login-style.css', false);
-}
-add_action('login_enqueue_scripts', 'load_aton_login_style');
-
 include_once 'aton-dashboard.php';
 include_once 'aton-actions.php';
 include_once 'aton-filters.php';
